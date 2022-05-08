cdd73d7942eb26021bd1300cacf2a520b1dcf650
pseudotsuga-fir <hepworta@oregonstate.edu>
2022-05-07 18:47:54 -0700

    refactor draw_button and Button using move function

--- main.rb
+++ main.rb
@@ -6,14 +6,18 @@ class Button(label_text, x, y, foreground_color, is_dark_mode)
     @foreground_color = foreground_color
     @is_dark_mode = is_dark_mode
   end
+
+  def paint(theme_color)
+    paint(label_text, x, y, foreground_color, theme_color)
+  end
 end
 
 def draw_button(button)
   if button.is_dark_mode
     # darken foreground color for dark mode
-    paint(button.label_text, button.x, button.y, button.foreground_color - 10, '#111111')
+    button.paint('#111111')
   else
     # lighten foreground color for non-dark mode
-    paint(button.label_text, button.x, button.y, button.foreground_color + 10, '#E0E0E0')
+    button.paint('#E0E0E0')
   end
 end
