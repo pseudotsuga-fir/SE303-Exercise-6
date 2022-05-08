dbdfd1fddf470904de8562860dd8b85b37d0d754
pseudotsuga-fir <hepworta@oregonstate.edu>
2022-05-07 18:42:46 -0700

    refactor draw_button using introduce parameter object

--- main.rb
+++ main.rb
@@ -1,9 +1,19 @@
-def draw_button(label_text, x, y, foreground_color, is_dark_mode)
-  if is_dark_mode
+class Button(label_text, x, y, foreground_color, is_dark_mode)
+  def initialize
+    @label_text = label_text
+    @x = x
+    @y = y
+    @foreground_color = foreground_color
+    @is_dark_mode = is_dark_mode
+  end
+end
+
+def draw_button(button)
+  if button.is_dark_mode
     # darken foreground color for dark mode
-    paint(label_text, x, y, foreground_color - 10, '#111111')
+    paint(button.label_text, button.x, button.y, button.foreground_color - 10, '#111111')
   else
     # lighten foreground color for non-dark mode
-    paint(label_text, x, y, foreground_color + 10, '#E0E0E0')
+    paint(button.label_text, button.x, button.y, button.foreground_color + 10, '#E0E0E0')
   end
 end
